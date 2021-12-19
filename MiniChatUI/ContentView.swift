1fb11b8099a2babaa5026e3a802f274c9cc99930
XING <zhaoxing8325@gmail.com>
2021-12-18 23:22:50 -0800

    Initial Commit

---
+++ MiniChatUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MiniChatUI
+//
+//  Created by Xing Zhao on 2021/12/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MiniChatUI/MiniChatUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniChatUIApp.swift
+//  MiniChatUI
+//
+//  Created by Xing Zhao on 2021/12/18.
+//
+
+import SwiftUI
+
+@main
+struct MiniChatUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
